b054571b2939ed44c1d530404954e45792181cd4
Andrii <badekha@gmail.com>
2021-10-19 18:24:05 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import './App.scss';
-import './styles/general.scss';
-import { districts, jobPlaces } from './api/api';
-import { Form } from './components/Form';
+import { Header } from './components/Header/Header';
 
 const App = () => (
   <div className="App">
-    <div className="App__title">
-      Заявки на відвідання тренінгу для вчителів
-    </div>
-    <Form districts={districts} jobPlaces={jobPlaces} />
+    <Header />
   </div>
 );
 
---
+++ src/store/navItemsReducer.js
@@ -0,0 +1,24 @@
+export const navItemsReducer = () => (
+  [
+    {
+      id: '1',
+      text: 'Company',
+      idLink: '#info',
+    },
+    {
+      id: '2',
+      text: 'Services',
+      idLink: '#gallery',
+    },
+    {
+      id: '3',
+      text: 'Blog',
+      idLink: '#consultation',
+    },
+    {
+      id: '4',
+      text: 'Career',
+      idLink: '#testimonials',
+    },
+  ]
+);
