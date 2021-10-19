99f8f04ecfc0b3d6380870b3af7fa9fe42c8c0ba
Andrii <badekha@gmail.com>
2021-10-19 18:25:55 +0300

    Updates

---
+++ precache-manifest.9331086f78e006a6250f90e35f6e033f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aa5c453b718c570f9648fbe88559a8",
+    "url": "/Freysoft/index.html"
+  },
+  {
+    "revision": "530ae67c65ac9bdc9d38",
+    "url": "/Freysoft/static/css/main.4af63e7b.chunk.css"
+  },
+  {
+    "revision": "3a7ff63951f44d0f8a78",
+    "url": "/Freysoft/static/js/2.1d3b02f1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Freysoft/static/js/2.1d3b02f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530ae67c65ac9bdc9d38",
+    "url": "/Freysoft/static/js/main.53bf9cb5.chunk.js"
+  },
+  {
+    "revision": "26bf6a78c86c427722ca",
+    "url": "/Freysoft/static/js/runtime-main.5aff09ad.js"
+  },
+  {
+    "revision": "47e2e7e97762b8b8107be08dfa2c2a15",
+    "url": "/Freysoft/static/media/logo.47e2e7e9.svg"
+  },
+  {
+    "revision": "3388b539b3a8a44d64a3a204860eebc7",
+    "url": "/Freysoft/static/media/menu.3388b539.svg"
+  },
+  {
+    "revision": "e7fae1c651e9aedf34343955e61dd71e",
+    "url": "/Freysoft/static/media/phone.e7fae1c6.svg"
+  }
+]);
