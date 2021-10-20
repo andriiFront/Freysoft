f8547952011a8df328d512870b0d2929f0bb59a4
Andrii <badekha@gmail.com>
2021-10-20 09:53:16 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Header } from './components/Header/Header';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
